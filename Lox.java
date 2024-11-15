a9afe762d832ed62c87c5341820b3be458b084b5
Adriza Mishra <driz@Adrizas-MacBook-Pro.local>
2024-11-16 01:11:12 +0530

    working parser

--- Lox.java
+++ Lox.java
@@ -48,11 +48,19 @@ public class Lox {
     private static void run(String source) {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
+        Parser parser = new Parser(tokens);
+        Expr expression = parser.parse();
 
         //for now, just print the tokens
-        for(Token token : tokens){
-            System.out.println(token);
-        }
+//        for(Token token : tokens){
+//            System.out.println(token);
+//        }
+
+        //Stop if there was a syntax error
+        if(hadError) return;
+
+        System.out.println(new AstPrinter().print(expression));
+
     }
 
     static void error(int line, String message){
@@ -64,9 +72,12 @@ public class Lox {
         hadError = true;
     }
 
-
-
-
-
+    static void error(Token token, String message){
+        if(token.type == TokenType.EOF){
+            report(token.line, " at end", message);
+        } else {
+            report(token.line, " at '" + token.lexeme, "'" + message);
+        }
+    }
 
 }
